f6890f72f68ab9955b16563a8a54b9925c9a90e5
Parshva Runwal <runwalparshva3@gmail.com>
2022-06-20 00:26:24 +0530

    Axios server set-up with interceptors for 401, 403 and sending x-auth-token

---
+++ frontend/src/Server.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const Server = axios.create({
+  baseURL: "http://localhost:8000/",
+});
+
+Server.interceptors.request.use((axiosConfig) => {
+  axiosConfig["headers"]["x-auth-token"] = localStorage.getItem("x-auth-token");
+  return axiosConfig;
+});
+
+Server.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error?.response?.status === 401 || error?.response?.status === 403) {
+      window.location = "/auth/sign-up";
+      return;
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default Server;

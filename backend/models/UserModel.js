950eb07ca68ea9ec9ea42ac3bb01fe91edfa2665
Parshva Runwal <runwalparshva3@gmail.com>
2022-06-19 21:28:21 +0530

    User Schema

---
+++ backend/models/UserModel.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true, trim: true },
+    lastName: { type: String, required: true, trim: true },
+    username: { type: String, required: true, trim: true, unique: true },
+    email: { type: String, required: true, trim: true, unique: true },
+    password: { type: String, required: true },
+    profilePic: { type: String, default: "/assets/images/defaultPFP.png" },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const UserModel = mongoose.model("User", UserSchema);
+module.exports = UserModel;

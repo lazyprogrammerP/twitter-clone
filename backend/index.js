0ed4f0e3c1b0f161b8b75d7a830d34b16f34b1ed
Parshva Runwal <runwalparshva3@gmail.com>
2022-06-20 00:19:38 +0530

    Replacing user sessions with JWT

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,28 @@
 const express = require("express");
 const app = express();
 
-app.use(express.json());
-
-const session = require("express-session");
+const cors = require("cors");
 app.use(
-  session({
-    secret: "rick-and-morty",
-    resave: true,
-    saveUninitialized: false,
+  cors({
+    credentials: true,
   })
 );
 
+app.use(express.json());
+
+const validateJWT = require("./middlewares/validateJWT");
+
 const Database = require("./database");
-const validateSession = require("./middlewares/validateSession");
-new Database(
-  `mongodb+srv://admin:admin123@cluster0.c1lai.mongodb.net/?retryWrites=true&w=majority`
-);
+new Database(`mongodb+srv://admin:admin123@cluster0.c1lai.mongodb.net/?retryWrites=true&w=majority`);
 
 const PORT = 8000;
-
-app.get("/", validateSession, (req, res) => {
-  res.status(200).send("Server is up and running!");
-});
 
 // Auth Routes
 const authRoutes = require("./routes/authRoutes");
 app.use("/auth", authRoutes);
+
+app.get("/", validateJWT, (req, res) => {
+  res.status(200).send("Server is up and running!");
+});
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));

dda858c25ef1a0037c25c5ccdbfb2de74c7bdbb6
Parshva Runwal <runwalparshva3@gmail.com>
2022-06-24 18:33:21 +0530

    Components moved to 'components' folder under src

---
+++ frontend/src/components/TailwindButton/TailwindButton.js
@@ -0,0 +1,20 @@
+import { FadingCircle } from "better-react-spinkit";
+
+const TailwindButton = ({ label, type, disabled, loading }) => {
+  return (
+    <button type={type} className={`flex items-center justify-center w-full ${disabled || loading ? `bg-gray-700` : `bg-blue-500`} text-gray-200 p-2 rounded-md font-semibold`} disabled={disabled || loading}>
+      <span>{label}</span>
+      {loading && (
+        <FadingCircle
+          size={"16px"}
+          color={"white"}
+          style={{
+            marginLeft: "12px",
+          }}
+        />
+      )}
+    </button>
+  );
+};
+
+export default TailwindButton;

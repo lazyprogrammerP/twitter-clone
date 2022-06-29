203eb68f8e22dc53ee36c056ec8ee7ff7ae1572c
Parshva Runwal <runwalparshva3@gmail.com>
2022-06-30 00:14:55 +0530

    Button size variant added

--- frontend/src/components/TailwindButton/TailwindButton.js
+++ frontend/src/components/TailwindButton/TailwindButton.js
@@ -1,11 +1,11 @@
 import { FadingCircle } from "better-react-spinkit";
 
-const TailwindButton = ({ label, type, disabled, loading, onClick }) => {
+const TailwindButton = ({ label, type, disabled, loading, onClick, size }) => {
   return (
     <button
       type={type}
       onClick={onClick}
-      className={`flex items-center justify-center w-full ${disabled || loading ? `bg-gray-700` : `bg-blue-500`} text-gray-200 p-2 rounded-md font-semibold`}
+      className={`flex items-center justify-center w-full ${disabled || loading ? `bg-gray-700` : `bg-blue-500`} text-gray-200 p-2 rounded-md font-semibold ${size === "small" ? "p-1" : ""}`}
       disabled={disabled || loading}
     >
       <span>{label}</span>

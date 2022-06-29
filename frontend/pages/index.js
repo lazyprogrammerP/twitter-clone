e86ac2f1a8aa8b1ebbab54493ec2327ff6a8a8d3
Parshva Runwal <runwalparshva3@gmail.com>
2022-06-29 22:48:50 +0530

    Created layout for homepage using the newly created sidebar and header components

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,10 +1,20 @@
-import { useEffect } from "react";
-import Server from "../src/Server";
+import Header from "../src/components/layout/Header";
+import Sidebar from "../src/components/layout/Sidebar";
 
 export default function Home() {
-  useEffect(() => {
-    Server.get("").then(console.log).catch(console.log);
-  }, []);
+  return (
+    <div className={"w-full min-h-screen bg-gray-900 text-gray-200"}>
+      <div className={"mx-auto max-w-6xl flex items-start"}>
+        <div>
+          <Sidebar />
+        </div>
 
-  return <div>Home Page</div>;
+        <div className={"middle-row"}>
+          <Header pageName={"Home"} />
+        </div>
+
+        <div className={"w-72 h-screen overflow-auto border-l-2 border-l-gray-800 hidden lg:block"}></div>
+      </div>
+    </div>
+  );
 }

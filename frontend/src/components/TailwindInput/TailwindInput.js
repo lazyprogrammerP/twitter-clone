45f8a40c078c2f39ea74ea4a65fb193f08a66da6
Parshva Runwal <runwalparshva3@gmail.com>
2022-06-30 01:25:50 +0530

    Multiline waala error fixed and textAreaRef added

--- frontend/src/components/TailwindInput/TailwindInput.js
+++ frontend/src/components/TailwindInput/TailwindInput.js
@@ -1,7 +1,7 @@
-const TailwindInput = ({ name, type, placeholder, required, Icon, size, classes = {}, multiline, rows }) => {
+const TailwindInput = ({ name, type, placeholder, required, Icon, size, classes = {}, multiline, rows, textAreaRef }) => {
   return (
     <div>
-      {multiline ? (
+      {!multiline ? (
         <div className={`flex items-center p-2 rounded-md bg-gray-800 ${classes.inputWrapper}`}>
           {Icon && <Icon className={`text-gray-500 w-5 h-5 mr-2 ${classes.iconClass}`} />}
           <input name={name} type={type} placeholder={placeholder} required={required} className={`${size === "small" ? "text-xs" : "text-sm"} focus:outline-none w-full bg-transparent ${classes.input}`} />
@@ -9,13 +9,14 @@ const TailwindInput = ({ name, type, placeholder, required, Icon, size, classes 
       ) : (
         <div className={`flex items-center p-3 rounded-md bg-gray-800 ${classes.inputWrapper}`}>
           <span
+            ref={textAreaRef}
             role={"textbox"}
             contentEditable
             className={`${size === "small" ? "text-xs" : "text-sm"} focus:outline-none w-full bg-transparent ${classes.input}`}
             style={{
               minHeight: `${20 * rows}px`,
             }}
-          ></span>
+          />
         </div>
       )}
     </div>

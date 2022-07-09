5095b911646e5e43add349a90d9a8e041a7c41a0
Parshva Runwal <runwalparshva3@gmail.com>
2022-07-09 21:01:47 +0530

    Sidebar absolute positioning fixed

--- frontend/src/components/layout/Sidebar.js
+++ frontend/src/components/layout/Sidebar.js
@@ -53,17 +53,17 @@ const sidebarItems = [
 const Sidebar = () => {
   const { user } = useUser();
   return (
-    <div className={"overflow-auto p-4 pb-2 flex justify-between border-r-gray-800 absolute bottom-0 w-full border-t-2 border-t-gray-800 md:static md:w-72 md:border-t-0 md:border-r-2 md:flex-col md:h-screen"}>
+    <div className={"w-72 h-screen overflow-auto p-4 pb-2 flex flex-col justify-between border-r-2 border-r-gray-800 absolute -left-72 md:static"}>
       {/* Upper Sidebar */}
       <div className={"w-full"}>
         <div className={"hidden md:block"}>
           <LogoSVG />
         </div>
 
-        <div className={"w-full flex items-center justify-around md:items-start md:my-4 md:flex-col md:gap-2"}>
+        <div className={"hidden md:flex w-full flex-col items-start justify-around my-4 gap-2"}>
           {sidebarItems.map(({ id, link, Icon, name, active }) => (
             <Link key={id} href={link}>
-              <div className={"w-max rounded-md cursor-pointer flex items-center gap-2 transition-colors duration-200 hover:bg-gray-600 hover:bg-opacity-30 p-1 md:p-2"}>
+              <div className={"w-max rounded-md cursor-pointer flex items-center gap-2 transition-colors duration-200 hover:bg-gray-600 hover:bg-opacity-30 p-2"}>
                 <Icon
                   className={"w-6 h-6 sm:w-8 sm:h-8"}
                   style={{

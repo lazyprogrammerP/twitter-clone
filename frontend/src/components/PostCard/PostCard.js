cd2aecf45d42cd718300bc5b16d1c445e5de5b6e
Parshva Runwal <runwalparshva3@gmail.com>
2022-07-13 01:35:16 +0530

    Like functionality added to the frontend PostCard

--- frontend/src/components/PostCard/PostCard.js
+++ frontend/src/components/PostCard/PostCard.js
@@ -1,7 +1,13 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useSnackbar } from "react-simple-snackbar";
 import { Heart, MessageCircle, Refresh } from "tabler-icons-react";
+import useUser from "../../hooks/useUser";
+import usePostsReducer from "../../redux/utils/usePostsReducer";
+import Server from "../../Server";
 import getTimeDifference from "../../utils/getTimeDifference";
 
-const PostActionButton = ({ color, Icon, value, label }) => {
+const PostActionButton = ({ color, Icon, fill = "transparent", value, label, onClick }) => {
   const colorDictionary = {
     red: "text-red-300 hover:bg-red-300",
     green: "text-green-300 hover:bg-green-300",
@@ -9,8 +15,8 @@ const PostActionButton = ({ color, Icon, value, label }) => {
   };
 
   return (
-    <button className={`flex items-center gap-2 p-2 transition-all duration-200 rounded-md ${colorDictionary[color]} hover:bg-opacity-20 cursor-pointer`}>
-      <Icon />
+    <button onClick={onClick} className={`flex items-center gap-2 p-2 transition-all duration-200 rounded-md ${colorDictionary[color]} hover:bg-opacity-20 cursor-pointer`}>
+      <Icon fill={fill} />
 
       <div className={"text-sm"}>
         <span>{value}</span>
@@ -54,7 +60,40 @@ export const PostCardSkeleton = () => {
   );
 };
 
-const PostCard = ({ content, postedBy, updatedAt }) => {
+const PostCard = ({ content, postedBy, createdAt, _id, idx, likes = [] }) => {
+  const posts = useSelector((state) => state.posts.posts);
+  const { setPosts } = usePostsReducer();
+
+  const { user } = useUser();
+  const [likedByUser, setLikedByUser] = useState(false);
+
+  const [openError] = useSnackbar({ style: { backgroundColor: "red", color: "white", fontFamily: "monospace" } });
+
+  const updatePost = (updatedPost) => {
+    setPosts([...posts.slice(0, idx), updatedPost, ...posts.slice(idx + 1, posts.length)]);
+  };
+
+  const onLike = () => {
+    updatePost({
+      ...posts[idx],
+      likes: likedByUser ? likes.filter((id) => id !== user?._id) : [...likes, user?._id],
+    });
+
+    Server.put(`api/posts/${_id}/like`)
+      .then((res) => {
+        updatePost({
+          ...res.data,
+        });
+      })
+      .catch((error) => {
+        openError(error?.response?.data?.message || error.message || "Something unexpected went wrong.");
+      });
+  };
+
+  useEffect(() => {
+    setLikedByUser(likes.includes(user?._id));
+  }, [user, likes]);
+
   return (
     <div className={"w-full p-4 border-b-2 border-b-gray-800"}>
       <div className={"flex items-start gap-2"}>
@@ -73,7 +112,7 @@ const PostCard = ({ content, postedBy, updatedAt }) => {
               <p className={"text-xs"}>@{postedBy.username}</p>
             </div>
             &#183;
-            <p className={"text-sm text-gray-500"}>{getTimeDifference(new Date(updatedAt))} ago</p>
+            <p className={"text-sm text-gray-500"}>{getTimeDifference(new Date(createdAt))} ago</p>
           </div>
 
           {/* Postcard Body */}
@@ -81,7 +120,7 @@ const PostCard = ({ content, postedBy, updatedAt }) => {
 
           {/* Postcard Action */}
           <div className={"flex items-center justify-between mt-3"}>
-            <PostActionButton Icon={Heart} value={0} label={"Likes"} color={"red"} />
+            <PostActionButton Icon={Heart} fill={likedByUser ? "rgb(252, 165, 165)" : "transparent"} value={likes.length} label={"Likes"} color={"red"} onClick={onLike} />
             <PostActionButton Icon={MessageCircle} value={0} label={"Replies"} color={"green"} />
             <PostActionButton Icon={Refresh} value={0} label={"Retweets"} color={"gray"} />
           </div>
